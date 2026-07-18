ddd7cbd2c13720bbffe4c78ebe05d549b91a18c5
Fotis Evangelou <fevangelou@users.noreply.github.com>
2026-07-18 16:46:28 +0300

    Rename license file in all copyrights from txt to md

--- administrator/components/com_languages/models/rules/languageoverridequotes.php
+++ administrator/components/com_languages/models/rules/languageoverridequotes.php
@@ -4,7 +4,7 @@
  * @subpackage  com_languages
  *
  * @copyright   (C) 2026 Open Source Matters, Inc. <https://www.joomla.org>
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @license     GNU General Public License version 2 or later; see LICENSE.md
  */
 
 defined('JPATH_PLATFORM') or die;
